ce08d43c500f0bce38162b5b586022483e4f8183
Carl Hemsle <carl@hemsle.se>
2025-08-21 12:48:19 +0200

    5 frågor

--- www/public/evaluate.php
+++ www/public/evaluate.php
@@ -7,6 +7,10 @@
 <?php
      	$ans1 = $_POST['q1'];
      	$ans2 = $_POST['q2'];
+		$ans3 = $_POST['q3'];
+     	$ans4 = $_POST['q4'];
+		$ans5 = $_POST['q5'];
+
         $name = $_POST['name'];
      	$points = 0;
      	
@@ -14,8 +18,14 @@
            $points++;
      	if($ans2 == 15)
            $points++;
+		if($ans3 == 50)
+           $points++;
+     	if($ans4 == 3)
+           $points++;
+		if($ans5 == 4)
+           $points++;
      	
-     	echo("<p>" . $name . " fick " . $points . " av 2 möjliga</p>");
+     	echo("<p>" . $name . " fick " . $points . " av 5 möjliga</p>");
 ?>
 </body>
 </html>
--- www/public/index.php
+++ www/public/index.php
@@ -12,6 +12,15 @@
        <label>3 x 5 = </label>
        <input type="number" name="q2">
        <br>
+       <label>10 x 5 = </label>
+       <input type="number" name="q3">
+       <br>
+       <label>12 / 4 = </label>
+       <input type="number" name="q4">
+       <br>
+       <label>2 + 2 = </label>
+       <input type="number" name="q5">
+       <br>
        <label>Namn</label>
        <input type="text" name="name">
        <br>
